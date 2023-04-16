c19b1ca5a91d73f3d8e314e7a67a36fdd0832698
djachenkoroman <emessage@rambler.ru>
2023-04-16 19:28:16 +0300

    Подправил pcvis.py

--- pcvis.py
+++ pcvis.py
@@ -6,6 +6,32 @@ import plotly.express as px
 def test():
     print("Hello World!")
 
+
+def preprocess(data_root, data_dir, GRID_SIZE):
+    os.makedirs(data_dir, exist_ok=False)
+    data = np.loadtxt(data_root)
+    classes = set(data[:, -1])
+    num_classes = len(classes)
+    idx = 0
+    x = data[:, 0]
+    y = data[:, 1]
+    x_max = int(np.max(x)) + 1
+    x_min = int(np.min(x)) - 1
+    y_max = int(np.max(y)) + 1
+    y_min = int(np.min(y)) - 1
+    del x
+    del y
+
+    for i in range(x_min, x_max - GRID_SIZE, GRID_SIZE):
+        for j in range(y_min, y_max - GRID_SIZE, GRID_SIZE):
+            arr = data[
+                (data[:, 0] > i) & (data[:, 0] < i + GRID_SIZE) & (data[:, 1] > j) & (data[:, 1] < j + GRID_SIZE)]
+            np.save(f'{data_dir}/{idx}.npy', arr)
+            idx += 1
+    del data
+    return num_classes, classes
+
+
 def visualize_rotate(data):
     x_eye, y_eye, z_eye = 1.25, 1.25, 0.8
     frames = []
